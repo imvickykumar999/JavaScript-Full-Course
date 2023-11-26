2e3253e75aad772f49ae49de37cd44b4a08d0dfb
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-26 16:28:06 +0530

    comparison

---
+++ Lecture2 (Operators & Conditionals)/script.js
@@ -0,0 +1,53 @@
+
+// This code prints hello world!
+console.log('hello world!');
+
+let a = 5;
+let b = 2;
+console.log("a = ", a, ", b = ", b);
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+
+console.log("a % b = ", a % b); // remainder division
+console.log("a ** b = ", a ** b); // power, 5^2 = 25
+
+console.log("a-- = ", a--, ", b++ = ", b++);
+console.log("a = ", a, ", b = ", b);
+
+console.log("--a = ", --a, ", ++b = ", ++b);
+console.log("a = ", a, ", b = ", b);
+
+a -= 4; // a = a - 4
+console.log("a = ", a);
+
+b += 3; // b = b + 3
+console.log("b = ", b);
+
+a = "7";
+console.log("typeof a = ", typeof a, ", typeof b = ", typeof b);
+
+// while comparing number in string explicitly converts to number.
+console.log("a == b", a == b);
+
+a++;
+b = "8";
+
+console.log("typeof a = ", typeof a);
+console.log("typeof b = ", typeof b);
+
+console.log("a = ", a, ", b = ", b);
+console.log("a == b", a == b);
+console.log("a != b", a != b);
+
+console.log("a === b", a === b);
+console.log("a !== b", a !== b);
+
+a++;
+b++;
+console.log("a = ", a, ", b = ", b);
+
+console.log("a < b", a < b);
+console.log("a > b", a > b);
